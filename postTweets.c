a2ee82fb16eb1c00cd97dffb68168c0dbabe6a61
maryannailo <104728553+maryannailo@users.noreply.github.com>
2022-05-03 21:59:42 +0100

    Add files via upload

---
+++ postTweets.c
@@ -0,0 +1,10 @@
+//
+// Created by Maryanna Iloduba on 03/05/2022.
+//
+#include <stdio.h>
+#include <string.h>
+#include "postTweets.h"
+#include "twitter_create.h"
+
+void postTweets(user* first, tweet* tweet1, twitter* twitter_system);
+
