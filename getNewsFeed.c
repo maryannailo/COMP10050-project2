a340216d542225ded6b2c8282ad469ab7bf49c8d
zeinakhattab <103892874+zeinakhattab@users.noreply.github.com>
2022-05-08 00:24:50 +0100

    Create getNewsFeed.c

---
+++ getNewsFeed.c
@@ -0,0 +1,42 @@
+//
+// Created by Zeina Khattab on 04/05/2022.
+//
+#include <stdio.h>
+#include <string.h>
+#include "twitter_create.h"
+
+void getNewsFeed(twitter *twitter_system, int tweetNumber,int user)
+{
+    for (int k = 0; k < tweetNumber; k++)
+    {
+        if (k >= 10)
+        {
+            break;
+        }
+        else
+        {
+            for (int i = 0; i < twitter_system->num_users; i++)
+            {
+                if (strcmp(twitter_system->users[user].username, twitter_system->news_feed[i].user) == 0)
+                {
+                    printf("User - %s:\nID : %d\nTweet : %s\n\n\n", twitter_system->news_feed[tweetNumber - k - 1].user,
+                           twitter_system->news_feed[tweetNumber - k - 1].id,
+                           twitter_system->news_feed[tweetNumber - k - 1].msg);
+                }
+                else
+                {
+                    for (int j = 0; j < twitter_system->users[user].num_following; j++)
+                    {
+                        if (strcmp(twitter_system->users[user].following[j], twitter_system->news_feed[i].user) == 0)
+                        {
+                            printf("User:%s:\nID: %d\nTweet:%s\n\n\n",
+                                   twitter_system->news_feed[tweetNumber - k - 1].user,
+                                   twitter_system->news_feed[tweetNumber - k - 1].id,
+                                   twitter_system->news_feed[tweetNumber - k - 1].msg);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
