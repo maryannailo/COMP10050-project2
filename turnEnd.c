239fb3ff23cb6666c83081368e9910b42f7d5d81
zeinakhattab <103892874+zeinakhattab@users.noreply.github.com>
2022-05-08 11:29:31 +0100

    Update turnEnd.c

--- turnEnd.c
+++ turnEnd.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include "twitter_create.h"
+
 void turnEnd(user* first){
     printf("User: %s\n---Followers: %d---\n---Following: %d---\n\n", first->username, first->num_followers, first->num_following);
 }
