bc0f2cb28281ddcc894a96c225bf4ca70ec47f19
zeinakhattab <103892874+zeinakhattab@users.noreply.github.com>
2022-05-08 01:31:25 +0100

    Update followUser.c

--- followUser.c
+++ followUser.c
@@ -1,11 +1,12 @@
-//
-//
+#include <stdio.h>
+#include <string.h>
+#include "twitter_create.h"
 
 void followUser(user* first, user* next){
     strcpy(first->following[first->num_following], next->username);
     first -> num_following++;
     strcpy(next->followers[next ->num_followers], first->username);
-    next->num_followers++
+    next->num_followers++;
 
     printf("You are following: %s\n", next->username);
 }
