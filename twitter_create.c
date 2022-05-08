7b1b7eddda51b45c624e4401afe633a27a417d86
zeinakhattab <103892874+zeinakhattab@users.noreply.github.com>
2022-05-08 13:19:57 +0100

    Update twitter_create.c

--- twitter_create.c
+++ twitter_create.c
@@ -1,14 +1,14 @@
-
 //
 // Created by Lili on 30/03/2022.
 //
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "twitter_create.h"
 
-void create_twitter_system(twitter * twitter_system){
+user* create_twitter_system(twitter * twitter_system){
 
     //to be completed
-
+    return calloc(1,sizeof(user));
 }
