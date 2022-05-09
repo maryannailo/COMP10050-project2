721e9eb6e5162d0b9b47deb4698f8b7f54032ef4
zeinakhattab <103892874+zeinakhattab@users.noreply.github.com>
2022-05-09 17:22:40 +0100

    Update unfollowUser.c

--- unfollowUser.c
+++ unfollowUser.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include "twitter_create.h"
 
-void followUser(user* first, user* next){
+void unfollowUser(user* first, user* next){
     strcpy(first->following[first->num_following], next->username);
     first -> num_following--;
     strcpy(next->followers[next ->num_followers], first->username);
