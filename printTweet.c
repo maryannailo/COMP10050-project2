4000911985a2216dad8fd475ac4af4d41b39fd6c
zeinakhattab <103892874+zeinakhattab@users.noreply.github.com>
2022-05-10 15:43:26 +0100

    Update printTweet.c

--- printTweet.c
+++ printTweet.c
@@ -1,9 +1,11 @@
 //
 // Created by MaryAnna Iloduba
 //
+#include <stdio.h>
+#include "twitter_create.h"
 
 void printTweet(twitter* twitter_system){
-    for (int i = 0; i < twitter_system->num_sweets; i++) {
+    for (int i = 0; i < twitter_system->num_tweets; i++) {
         printf("%s", &twitter_system->news_feed[i].msg);
     }
 }
