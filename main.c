bba2c4481611409679c5e7f58c1ceebc20b001a0
maryannailo <104728553+maryannailo@users.noreply.github.com>
2022-05-24 16:30:21 +0100

    Update main.c
    using  switch statement instead of many if statements

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 
 int main()
 {
-    int input, n1, n2=0; //n1: following count, n2: tweet count
+    int input;
+    int n1, n2=0; //n1: following count, n2: tweet count
+    
     twitter twitter_system; //declaring twitter system struct
     create_twitter_system(&twitter_system); //function call to create twitter system
 
@@ -16,54 +18,54 @@ int main()
     //for loop to run program. loops through each user and asks what function they would like to do
     for(int j=0; j<twitter_system.num_users; j++)
     {
-        n1 = 0; //initialise the following count to zero for each user
-        printf("User %d - %s : Pick one of the following:\n1: Post a tweet\n2: Return the News Feed\n"
-               "3: Follow another user\n4: Unfollow a user\n5: Move to the next user\n"
-               "6: Terminate the program\n", j, twitter_system.users[j].username);
+        n1 = 0; // the following count to zero for each user at first
+        printf("User %d - %s : Please choose an option from 1 - 6:\n1: Post a tweet\n2: Return the News Feed\n"
+               "3: Follow another user\n4: Unfollow a user\n5: Delete your user\n6: Move to the next user\n", j, twitter_system.users[j].username);
 
         scanf("%d%*c", &input); //asks what function the users want to use and assign it to the input variable
-        while (input > 7 || input < 1)
+        while (input < 1 || input > 7)
         {
             printf("Invalid choice. Please input a valid number.\n"); //printed if an invalid choice is made
             scanf("%d%*c", &input);
-        }
+        }.
 
         while(input != 7) //while loop as long as the input is not equal to 7
         {
-            //if statements to call the correct function according to the user's input
-            if(input == 1)
+            //swithch statements to call the correct function according to the user's input
+            switch(input)
             {
+                case 1:
                 postTweets(j, n2, &twitter_system); //The user posts a tweet
                 n2++; //increment number of tweets
-            }
-            else if(input == 2)
-            {
+                break;
+                    
+                case 2:
                 getNewsFeed(&twitter_system, n2, j); //users News Feed is printed
-            }
-            else if(input == 3)
-            {
+                break;
+                
+                case 3:
                 followUser(&twitter_system, j); //The user follows another user
                 n1++; //increment number of followers
-            }
-            else if(input == 4)
-            {
+                break;
+                    
+                case 4:
                 unfollowUser(n1, j); //The user unfollows a user they follow
-            }
-            else if(input == 5)
-            {
+                break;
+
+                case 5:
                 delete(j, &twitter_system); //The user deletes their account from the twitter system
-            }
-            else if(input == 6)
-            {
+                break;
+                    
+                case 6:
                 break; //program moves to the next user
             }
 
-            printf("User %d - %s : Pick one of the following:\n1: Post a tweet\n2: Return the News Feed\n"
-                   "3: Follow another user\n4: Unfollow a user\n5: Move to the next user\n"
-                   "6: Terminate the program\n", j, twitter_system.users[j].username);
+            printf("User %d - %s : Please choose another option from 1 - 7:\n1: Post a tweet\n2: Return the News Feed\n"
+               "3: Follow another user\n4: Unfollow a user\n5: Delete your user\n6: Move to the next user\n"
+               "7: Terminate the program/End Twitter\n", j, twitter_system.users[j].username);
 
             scanf("%d%*c", &input); //asks what function the users wants to use next
-            while (input > 7 || input < 1)
+            while (input < 1 || input > 7)
             {
                 printf("Invalid choice. Please input a valid number.\n"); //if an invalid choice is made this is printed
                 scanf("%input%*c", &input);
